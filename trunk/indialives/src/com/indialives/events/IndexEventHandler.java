41c23f079be089f737ffd9def3a67faa9feb20c0
vijayan.srinivasan <vijayan.srinivasan@79b9e600-9ba1-11de-b55d-05c6a1a974a6>
2009-09-07 11:36:02 +0000

    India Lives Project stated in Google code

---
+++ trunk/indialives/src/com/indialives/events/IndexEventHandler.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.indialives.events;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.easymvc.eventhandler.EventHandler;
+import com.easymvc.eventhandler.NonAuthenticatedHandler;
+
+/**
+ * @author Vijayan Srinivasan
+ * @since Sep 7, 2009 4:24:55 PM
+ */
+public class IndexEventHandler implements NonAuthenticatedHandler, EventHandler{
+
+	@Override
+	public void forward(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		RequestDispatcher dispatcher=request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	@Override
+	public void process(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+	}
+
+}
