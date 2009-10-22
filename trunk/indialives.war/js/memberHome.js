bbd7bb9c13f230e2a4bcc28b5999354f10ce5038
gopinath.dhanapal <gopinath.dhanapal@79b9e600-9ba1-11de-b55d-05c6a1a974a6>
2009-10-22 13:32:48 +0000

    -Provided form validation for needed pages.

--- trunk/indialives.war/js/memberHome.js
+++ trunk/indialives.war/js/memberHome.js
@@ -1,5 +1,15 @@
 function  submitCommunity(){
+	var gatedCommunityId=document.getElementById("gatedCommunityId").checked;
+	var radioChecked=false;
+	for(var i=0;i<document.memberHome.gatedCommunityId.length;i++){		
+		if(document.memberHome.gatedCommunityId[i].checked==true){
+			radioChecked=true;			
+		}		
+	}	
 	
-		document.memberHome.submit();
-	
+	if(!radioChecked){
+		alert("Please check any one of the radio button");	
+	}else{
+		document.memberHome.submit();		
+	}	
 }
