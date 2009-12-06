7335fc4500591f46249ee7935e8ad6edadf55b81
hivenki <hivenki@79b9e600-9ba1-11de-b55d-05c6a1a974a6>
2009-12-06 06:51:01 +0000

    Event handlers for Ameneties, Payments, Security, VAS and Visitors.

---
+++ indialives.war/src/com/indialives/events/PaymentsEventHandler.java
@@ -0,0 +1,33 @@
+package com.indialives.events;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.easymvc.eventhandler.EventHandler;
+import com.easymvc.persistence.RowObject;
+import com.indialives.PageNameConstants;
+import com.indialives.SetAttributeConstants;
+
+public class PaymentsEventHandler implements EventHandler, SetAttributeConstants, PageNameConstants {
+	
+	
+	List<RowObject> managementTaskList=null;
+	public void forward(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		RequestDispatcher dispatcher=request.getRequestDispatcher(PAYMENTS_PAGE);
+		dispatcher.forward(request,response);
+	}
+
+	public void process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+	}
+	
+}
